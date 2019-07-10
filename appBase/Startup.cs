3e7220602f0a993efe36e537923b1c4133653b95
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-07-10 08:13:29 -0300

    Initial Commit
    +Adicionado a view Index;
    +Adicionado o controller App;
    +Configurado para utilizar o padrão MVC;
    +Injetado o serviço para utilizar o padrão MVC;
    +Declarado titulo com Razor;

--- appBase/Startup.cs
+++ appBase/Startup.cs
@@ -15,15 +15,27 @@ namespace appBase
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Injeta o serviço do padrão Mvc
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();//utiliza arquivos padrão como a index para ser página principal. (*A ordem da chamada dos métodos deve ser essa.)
+            //utiliza arquivos padrão como a index para ser página principal. (*A ordem da chamada dos métodos deve ser essa.)
+            //app.UseDefaultFiles(); //Desabilitei para usar o padrao MVC e não utilizar a página default do WWWRoot.
+
             //Quando recebe uma requisição, ele utiliza arquivos estáticos como resposta.
             //Eles somente funcionam dentro da pasta: wwwroot(Que é a raiz para web no asp.net core)
             app.UseStaticFiles();
+
+            //Adiciona a configuração do padrão MVC
+            app.UseMvc(cfg => 
+            {
+                cfg.MapRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
