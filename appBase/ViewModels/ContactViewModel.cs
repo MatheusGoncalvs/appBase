e59117567b1e3e46de9be6e63a93aa4f581c2ec5
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-07-15 07:25:40 -0300

    Small Changes

--- appBase/ViewModels/ContactViewModel.cs
+++ appBase/ViewModels/ContactViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace appBase.ViewModels
 {
     public class ContactViewModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Subject { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Message { get; set; }
     }
 }
