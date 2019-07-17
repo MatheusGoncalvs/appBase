8b0077e79499092742716c037ec5d1f21784e747
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-07-16 21:33:58 -0300

    Iniciando com o Entity framework - Adicionando a collection service e especificando o banco sqlServer

--- appBase/Startup.cs
+++ appBase/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using appBase.Data;
 using appBase.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace appBase
@@ -16,6 +18,11 @@ namespace appBase
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<appBaseContext>(cfg => 
+            {
+                cfg.UseSqlServer();
+            });
+
             services.AddTransient<INullMailService, NullMailService>();
             //Support Real mail service
 
